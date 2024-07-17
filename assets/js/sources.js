73af5aa0fd045abf8cbbdf49d0b0a4b852d93ba5
Hafizi Ruslan <pizi@hotmail.my>
2024-07-17 08:01:24 +0800

    load eruda onDemand

--- assets/js/sources.js
+++ assets/js/sources.js
@@ -127,3 +127,10 @@ function downloadFile(data, filename, mime) {
 		window.URL.revokeObjectURL(blobURL)
 	}, 100)
 }
+
+; (function () {
+	var src = '//cdn.jsdelivr.net/npm/eruda'
+	if (!/eruda=true/.test(window.location) && localStorage.getItem('active-eruda') != 'true') return
+	document.write(`<script src="${src}" crossorigin="anonymous"></script>`)
+	document.write(`<script>eruda.init();</script>`)
+})()
