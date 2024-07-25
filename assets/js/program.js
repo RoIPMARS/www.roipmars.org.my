50f5ea67a214cbfb7848ab902be0a83fb08cf64b
Hafizi Ruslan <pizi@hotmail.my>
2024-07-25 23:53:21 +0800

    load latest bg_img; increase displayStart

--- assets/js/program.js
+++ assets/js/program.js
@@ -78,7 +78,7 @@ $(document).ready(function () {
 			{ searchable: false, targets: -1 },
 		],
 		deferRender: true,
-		displayStart: 460,
+		displayStart: 470,
 		ordering: false,
 		lengthChange: false,
 		pageLength: 10,
