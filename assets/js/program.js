6c8ca69514c51af911c78ae27f033dd539c7e34c
Hafizi Ruslan <pizi@hotmail.my>
2024-08-06 04:51:54 +0800

    increase displaystart

--- assets/js/program.js
+++ assets/js/program.js
@@ -78,7 +78,7 @@ $(document).ready(function () {
 			{ searchable: false, targets: -1 },
 		],
 		deferRender: true,
-		displayStart: 470,
+		displayStart: 480,
 		ordering: false,
 		lengthChange: false,
 		pageLength: 10,
