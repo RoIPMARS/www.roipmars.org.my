fc343d16e6b685b8b7f146cdcc86b055aee483ca
Hafizi Ruslan <pizi@hotmail.my>
2024-09-12 04:10:24 +0800

    displayStart: 510

--- assets/js/program.js
+++ assets/js/program.js
@@ -78,7 +78,7 @@ $(document).ready(function () {
 			{ searchable: false, targets: -1 },
 		],
 		deferRender: true,
-		displayStart: 500,
+		displayStart: 510,
 		ordering: false,
 		lengthChange: false,
 		pageLength: 10,
