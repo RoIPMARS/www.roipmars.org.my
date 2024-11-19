4769ceca29105a918863ac34fdfd95628c82c91f
RoIPMARS Records <records@roipmars.org.my>
2024-11-19 08:01:35 +0800

    increase displayStart 2024-11-19T08:00:50.027+08:00

--- assets/js/program.js
+++ assets/js/program.js
@@ -78,7 +78,7 @@ $(document).ready(function () {
 			{ searchable: false, targets: -1 },
 		],
 		deferRender: true,
-		displayStart: 540,
+		displayStart: 550,
 		ordering: false,
 		lengthChange: false,
 		pageLength: 10,
