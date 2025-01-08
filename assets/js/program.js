3c0faf123753a6d4ddb4d8e9ff12bd9880d9b220
RoIPMARS Records <records@roipmars.org.my>
2025-01-08 08:02:36 +0800

    increase displayStart 2025-01-08T08:00:53.025+08:00

--- assets/js/program.js
+++ assets/js/program.js
@@ -78,7 +78,7 @@ $(document).ready(function () {
 			{ searchable: false, targets: -1 },
 		],
 		deferRender: true,
-		displayStart: 570,
+		displayStart: 580,
 		ordering: false,
 		lengthChange: false,
 		pageLength: 10,
