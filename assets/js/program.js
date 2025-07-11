ef71794ede6654f4068f993299fd5cb8ff4a087a
RoIPMARS Records <records@roipmars.org.my>
2025-07-12 06:50:37 +0800

    increase displayStart #619057.bWN9qFeFepw6Nn1A

--- assets/js/program.js
+++ assets/js/program.js
@@ -78,7 +78,7 @@ $(document).ready(function () {
 			{ searchable: false, targets: -1 },
 		],
 		deferRender: true,
-		displayStart: 640,
+		displayStart: 650,
 		ordering: false,
 		lengthChange: false,
 		pageLength: 10,
